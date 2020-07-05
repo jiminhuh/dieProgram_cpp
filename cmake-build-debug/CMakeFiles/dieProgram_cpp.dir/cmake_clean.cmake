93388d26e554db2b25d7dba5bf5aa2e036f72a34
jimin huh <jiminhuh95@gmail.com>
2020-07-05 14:47:01 -0500

    repository initialized

---
+++ cmake-build-debug/CMakeFiles/dieProgram_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dieProgram_cpp.dir/main.cpp.obj"
+  "dieProgram_cpp.exe"
+  "dieProgram_cpp.exe.manifest"
+  "dieProgram_cpp.pdb"
+  "libdieProgram_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dieProgram_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
